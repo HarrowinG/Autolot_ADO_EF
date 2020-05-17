9687ef650be460775996e200b94d25a4b8e2f9ed
Vladislav Tetyuev <vladislavtetyuev@gmail.com>
2020-05-17 11:01:45 +0300

    finished basics EF from DB

--- AutoLot_ADO_EF_Core/AutoLotEfConsoleApp/Program.cs
+++ AutoLot_ADO_EF_Core/AutoLotEfConsoleApp/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using AutoLotEfConsoleApp.EF;
 
@@ -13,7 +15,10 @@ namespace AutoLotEfConsoleApp
             //            int carId = AddNewRecord();
             //            Console.WriteLine(carId);
             //GetShortCars();
-            PrintAllInventory();
+            //PrintAllInventory();
+            //LazyLoading();
+            //EagerLoading();
+            //RemoveRecordUsingEntityState(2);
             Console.ReadLine();
         }
 
@@ -74,6 +79,129 @@ namespace AutoLotEfConsoleApp
             {
                 //Find search in DbChangeTracker first and then in Db
                 Console.WriteLine(context.Cars.Find(id));
+            }
+        }
+
+        private static void ChainingLinqQueries()
+        {
+            using (var context = new AutoLotDbContext())
+            {
+                var allData = context.Cars;
+                var colorsMakes = from item in allData select new { item.Color, item.Make };
+
+                foreach (var item in colorsMakes)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+
+        private static void LazyLoading()
+        {
+            using (var context = new AutoLotDbContext())
+            {
+                context.Configuration.LazyLoadingEnabled = true; //by default
+                foreach (var car in context.Cars)
+                {
+                    //will hit database many times
+                    foreach (var order in car.Orders)
+                    {
+                        Console.WriteLine(order.OrderId);
+                    }
+                }
+            }
+        }
+
+        private static void EagerLoading()
+        {
+            using (var context = new AutoLotDbContext())
+            {
+                context.Configuration.LazyLoadingEnabled = false; //doesn't matter, we loaded all we need
+                foreach (var car in context.Cars.Include("Orders"))
+                {
+                    foreach (var order in car.Orders)
+                    {
+                        Console.WriteLine(order.OrderId);
+                    }
+                }
+            }
+        }
+
+        private static void ExplicitLoading()
+        {
+            using (var context = new AutoLotDbContext())
+            {
+                context.Configuration.LazyLoadingEnabled = false; //to guarantee nothing more loaded
+                foreach (var car in context.Cars)
+                {
+                    context.Entry(car).Collection(x => x.Orders).Load();
+                    foreach (var order in car.Orders)
+                    {
+                        Console.WriteLine(order.OrderId);
+                    }
+                }
+
+                foreach (var order in context.Orders)
+                {
+                    context.Entry(order).Reference(x => x.Car).Load();
+                }
+            }
+        }
+
+        private static void RemoveRecord(int carId)
+        {
+            using (var context = new AutoLotDbContext())
+            {
+                var carToDelete = context.Cars.Find(carId); //first load entity and then delete it
+                if (carToDelete != null)
+                {
+                    context.Cars.Remove(carToDelete);
+                }
+
+                Console.WriteLine(context.Entry(carToDelete).State); //Deleted
+                context.SaveChanges();
+            }
+        }
+
+        private static void RemoveMultipleRecords(IEnumerable<Car> carsToRemove)
+        {
+            using (var context = new AutoLotDbContext())
+            {
+                context.Cars.RemoveRange(carsToRemove);
+                context.SaveChanges();
+            }
+        }
+
+        private static void RemoveRecordUsingEntityState(int carId)
+        {
+            using (var context = new AutoLotDbContext())
+            {
+                var carToDelete = new Car{ CarId = carId };
+                //var car = context.Cars.Find(carId);  //to fail
+                context.Entry(carToDelete).State = EntityState.Deleted; //will fail if item with the same key already tracked
+                try
+                {
+                    context.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException ex) //will fail if no entity with such Id
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+        }
+
+        private static void UpdateRecord(int carId)
+        {
+            using (var context = new AutoLotDbContext())
+            {
+                var carToUpdate = context.Cars.Find(carId);
+                if (carToUpdate != null)
+                {
+                    Console.WriteLine(context.Entry(carToUpdate).State);
+                    carToUpdate.Color = "Blue";
+                    Console.WriteLine(context.Entry(carToUpdate).State);
+                    context.SaveChanges();
+                }
             }
         }
     }
