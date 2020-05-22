25cc39bcd16055560a1b5099c1292f6fbb693c19
Vladislav Tetyuev <vladislavtetyuev@gmail.com>
2020-05-22 15:42:45 +0300

    ObjectContext interception

--- AutoLot_ADO_EF_Core/EF/AutoLotDAL/EF/AutoLotDbContext.cs
+++ AutoLot_ADO_EF_Core/EF/AutoLotDAL/EF/AutoLotDbContext.cs
@@ -1,3 +1,5 @@
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
 using System.Data.Entity.Infrastructure.Interception;
 using AutoLotDAL.Interception;
 using AutoLotDAL.Models;
@@ -19,6 +21,39 @@ namespace AutoLotDAL.EF
             DbInterception.Add(ConsoleWriterInterceptor);
             DatabseLogger.StartLogging();
             DbInterception.Add(DatabseLogger);
+
+            var context = (this as IObjectContextAdapter).ObjectContext;
+            //just after properties of object is set and before object returned from context
+            context.ObjectMaterialized += OnObjectMaterialized;
+            //just after starting to push values to DB but before update DB
+            context.SavingChanges += OnSavingChanges;
+        }
+
+        //reject if color Red
+        private void OnSavingChanges(object sender, EventArgs e)
+        {
+            //sender is ObjectContext
+            var context = sender as ObjectContext;
+            if (context == null)
+            {
+                return;
+            }
+
+            foreach (var item in context.ObjectStateManager.GetObjectStateEntries(EntityState.Modified | EntityState.Added))
+            {
+                if ((item.Entity as Inventory) != null)
+                {
+                    var entity = (Inventory) item.Entity;
+                    if (entity.Color == "Red")
+                    {
+                        item.RejectPropertyChanges(nameof(entity.Color));
+                    }
+                }
+            }
+        }
+
+        private void OnObjectMaterialized(object sender, ObjectMaterializedEventArgs e)
+        {
         }
 
         public virtual DbSet<CreditRisk> CreditRisks { get; set; }
