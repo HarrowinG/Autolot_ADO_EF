9ec073fa05574d8994f0f57711e68d64f243e583
Vladislav Tetyuev <vladislavtetyuev@gmail.com>
2020-06-15 15:57:10 +0300

    initial host

---
+++ AutoLot_ADO_EF_Core/WCF/MagicBall/MagicEightBallServiceHost/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ServiceModel;
+using MagicEightBallServiceLib;
+
+namespace MagicEightBallServiceHost
+{
+    public class Program
+    {
+        //1. create endpoint in config
+        //2. create ServiceHost
+        //3. keep host running)
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Console Based WCF Host *****");
+            using (var serviceHost = new ServiceHost(typeof(MagicEightBallService)))
+            {
+                serviceHost.Open();
+
+                Console.WriteLine("Service is ready");
+                Console.WriteLine("Press the Enter key to terminate service.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
