d070abd166fd3e492367966c945347559a991ee0
Vladislav Tetyuev <vladislavtetyuev@gmail.com>
2020-06-15 17:07:01 +0300

    added simple client

---
+++ AutoLot_ADO_EF_Core/WCF/MagicBall/MagicEightBallServiceClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MagicEightBallServiceClient.ServiceReference1;
+
+namespace MagicEightBallServiceClient
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** Ask the Magic 8 Ball ***");
+
+            using (var ball = new EightBallClient())
+            {
+                Console.Write("Your question: ");
+                string question = Console.ReadLine();
+                string answer = ball.ObtainAnswerToQuestion(question);
+                Console.WriteLine($"8-Ball says: {answer}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
