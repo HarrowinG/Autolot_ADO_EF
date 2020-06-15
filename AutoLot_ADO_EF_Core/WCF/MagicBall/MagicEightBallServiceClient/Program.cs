131abab910f76f250aa1c2ad5b59f22292fe6cba
Vladislav Tetyuev <vladislavtetyuev@gmail.com>
2020-06-15 17:59:08 +0300

    config simplification and multiple bindings

--- AutoLot_ADO_EF_Core/WCF/MagicBall/MagicEightBallServiceClient/Program.cs
+++ AutoLot_ADO_EF_Core/WCF/MagicBall/MagicEightBallServiceClient/Program.cs
@@ -9,7 +9,8 @@ namespace MagicEightBallServiceClient
         {
             Console.WriteLine("*** Ask the Magic 8 Ball ***");
 
-            using (var ball = new EightBallClient())
+            //NetTcpBinding_IEightBall
+            using (var ball = new EightBallClient("BasicHttpBinding_IEightBall"))
             {
                 Console.Write("Your question: ");
                 string question = Console.ReadLine();
