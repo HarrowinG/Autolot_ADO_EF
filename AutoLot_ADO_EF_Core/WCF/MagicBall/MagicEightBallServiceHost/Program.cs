73da6263d18683ad6813791c2e5efb7c5f5f3650
Vladislav Tetyuev <vladislavtetyuev@gmail.com>
2020-06-15 16:16:07 +0300

    add info method for service host

--- AutoLot_ADO_EF_Core/WCF/MagicBall/MagicEightBallServiceHost/Program.cs
+++ AutoLot_ADO_EF_Core/WCF/MagicBall/MagicEightBallServiceHost/Program.cs
@@ -6,8 +6,9 @@ namespace MagicEightBallServiceHost
 {
     public class Program
     {
+        //0. Any WCF host run need Administrator privilege
         //1. create endpoint in config
-        //2. create ServiceHost
+        //2. create ServiceHost in IIS created automatically
         //3. keep host running)
         static void Main(string[] args)
         {
@@ -15,11 +16,27 @@ namespace MagicEightBallServiceHost
             using (var serviceHost = new ServiceHost(typeof(MagicEightBallService)))
             {
                 serviceHost.Open();
+                DisplayHostInfo(serviceHost);
 
                 Console.WriteLine("Service is ready");
                 Console.WriteLine("Press the Enter key to terminate service.");
                 Console.ReadLine();
             }
+        }
+
+        static void DisplayHostInfo(ServiceHost host)
+        {
+            Console.WriteLine();
+            Console.WriteLine("*** Host Info ***");
+
+            foreach (var se in host.Description.Endpoints)
+            {
+                Console.WriteLine($"Address: {se.Address}");
+                Console.WriteLine($"Binding: {se.Binding.Name}");
+                Console.WriteLine($"Contract: {se.Contract.Name}");
+                Console.WriteLine();
+            }
+            Console.WriteLine("*******");
         }
     }
 }
