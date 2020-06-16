832b59ef3b1dcbc544c848ab808762b69f42ae5e
Vladislav Tetyuev <vladislavtetyuev@gmail.com>
2020-06-16 17:53:43 +0300

    math service Win service host

---
+++ AutoLot_ADO_EF_Core/WCF/MathLib/MathServiceLibrary/MathService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+
+namespace MathServiceLibrary
+{
+    public class MathService : IBasicMath
+    {
+        public int Add(int x, int y)
+        {
+            Thread.Sleep(5000);
+            return x + y;
+        }
+    }
+}
---
+++ AutoLot_ADO_EF_Core/WCF/MathLib/MathWindowsServiceHost/MathService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceProcess;
+
+namespace MathWindowsServiceHost
+{
+    public partial class MathService : ServiceBase
+    {
+        private ServiceHost _myHost;
+
+        public MathService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            _myHost?.Close();
+//            _myHost = new ServiceHost(typeof(MathService));
+//            var address = new Uri("http://localhost:8080/MathServiceLibrary");
+//            var binding = new WSHttpBinding();
+//            var contract = typeof(IBasicMath);
+//            _myHost.AddServiceEndpoint(contract, binding, address);
+
+            var address = new Uri("http://localhost:8080/MathServiceLibrary");
+            _myHost = new ServiceHost(typeof(MathService), address);
+            _myHost.AddDefaultEndpoints();
+            _myHost.Open();
+        }
+
+        protected override void OnStop()
+        {
+            _myHost?.Close();
+        }
+    }
+}
